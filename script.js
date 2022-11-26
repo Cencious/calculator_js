7cd7f49c9a2f5b9c2f2565a45f9bacf4eaeecc43
Cencious <user.email>
2022-11-26 18:24:56 +0300

    add a function to display numbers with comas.

--- script.js
+++ script.js
@@ -53,11 +53,35 @@ class Calculator{
         this.operation = undefined 
         this.previousOperand = ''
     }
-    updateDisplay(){
-     this.currentOperandTextElement.innerText = this.currentOperand   
-     this.previousOperandTextElement.innerText = this.previousOperand  
-    }    
-}
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+          integerDisplay = ''
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`
+        } else {
+          return integerDisplay
+        }
+      }
+    
+    updateDisplay() {
+       this.currentOperandTextElement.innerText =
+         this.getDisplayNumber(this.currentOperand)
+       if (this.operation != null) {
+          this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+          this.previousOperandTextElement.innerText = ''
+        }
+      }
+    }
 
 
 
@@ -91,6 +115,11 @@ equalsButton.addEventListener('click', button => {
 })
 
 allClearButton.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
+    calculator.clear()
+    calculator.updateDisplay()
+})
+  
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
